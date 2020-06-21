8e9ed3a261c6ab29b1e37b42136e0cb2e97f5c97
Jérémy Leyvraz <32959624+JeremyLeyvraz@users.noreply.github.com>
2020-06-21 14:02:23 +0200

    Add DependencyInjection, object factory and a record singleton service method (#1)
    * Add DependencyInjection and record singleton method
    
    * Update summary

---
+++ InversionOfControl/InversionOfControl/DependencyInjection/Factory.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace InversionOfControl.DependencyInjection
+{
+    /// <summary>
+    /// Object factory
+    /// </summary>
+    public static class Factory
+    {
+        #region Public methods
+
+        /// <summary>
+        /// Construct an object
+        /// </summary>
+        /// <typeparam name="T">Type of the object to construct</typeparam>
+        /// <returns>An instance of the object of type T</returns>
+        /// <remarks>All parameters of the constructor of type must be register in the service registry <see cref="RegistrationService"/></remarks>
+        /// <remarks>The object T must have only one constructor</remarks>
+        public static T Construct<T>()
+        {
+            var allConstructors = typeof(T).GetConstructors();
+            var firstConstructor = allConstructors[0];
+            var paramList = new List<object>();
+            foreach (var param in firstConstructor.GetParameters())
+            {
+                var service = RegistrationService.Resolve(param);
+                paramList.Add(service);
+            }
+
+            return (T)firstConstructor.Invoke(paramList.ToArray());
+        }
+
+        #endregion
+    }
+}
---
+++ InversionOfControl/InversionOfControl/DependencyInjection/RegistrationService.cs
@@ -0,0 +1,61 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace InversionOfControl.DependencyInjection
+{
+    /// <summary>
+    /// Registration service
+    /// </summary>
+    public static class RegistrationService
+    {
+        #region Fields
+
+        /// <summary>
+        /// Recorded service collection
+        /// </summary>
+        private readonly static IServiceCollection serviceCollection = new ServiceCollection();
+
+        /// <summary>
+        /// Service provider
+        /// </summary>
+        private static ServiceProvider serviceProvider;
+
+        #endregion
+
+        #region Public methods
+
+        /// <summary>
+        /// Record a service as a singleton
+        /// </summary>
+        /// <param name="interfaceToRegister">Type information of interface type to register</param>
+        /// <param name="implentationToRegister">Type information of implementation type to register</param>
+        public static void RecordAsSingleton(TypeInfo interfaceToRegister, TypeInfo implentationToRegister)
+        {
+            serviceCollection.AddSingleton(interfaceToRegister, implentationToRegister);
+        }
+
+        /// <summary>
+        /// Build the service provider with all services recorded
+        /// </summary>
+        public static void BuildServiceProvider()
+        {
+            serviceProvider = serviceCollection.BuildServiceProvider();
+        }
+
+        #endregion
+
+        #region Internal methods
+
+        /// <summary>
+        /// Get the implementation of the recorded service
+        /// </summary>
+        /// <param name="param">ParameterInfo of the service interface to get</param>
+        /// <returns>Recorded implementation of the service interface to get</returns>
+        internal static object Resolve(ParameterInfo param)
+        {
+            return serviceProvider.GetRequiredService(param.ParameterType);
+        }
+
+        #endregion
+    }
+}
